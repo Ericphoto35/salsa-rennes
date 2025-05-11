7315b08f141351d7060299cc5ac1216620b204ac
Ericphoto35 <ericdu974@gmail.com>
2025-05-11 22:12:36 +0200

    Amélioration du SEO avec balises meta supplémentaires pour un meilleur référencement

--- components/Seo.js
+++ components/Seo.js
@@ -4,23 +4,41 @@ export default function Seo({
   title = 'Salsa Rennes',
   description = 'Le site de référence pour la salsa à Rennes : soirées, cours, événements, et plus encore.',
   url = 'https://www.salsarennes.fr',
-  image = '/images/og-image.jpg', // À personnaliser
+  image = '/images/clem-eric.webp', // Nom de fichier corrigé sans espace
+  imageWidth = 1200,
+  imageHeight = 630,
   type = 'website',
   noIndex = false,
+  keywords = 'salsa rennes, cours de salsa, danse latine, école de danse rennes',
 }) {
+  // URL complète de l'image
+  const fullImageUrl = `${url}${image}`;
+  
   return (
     <Head>
+      {/* Balises de base */}
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="language" content="fr" />
+      <meta httpEquiv="content-language" content="fr" />
+      
+      {/* Open Graph / Facebook */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content={type} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={fullImageUrl} />
+      <meta property="og:image:width" content={imageWidth.toString()} />
+      <meta property="og:image:height" content={imageHeight.toString()} />
+      <meta property="og:locale" content="fr_FR" />
+      <meta property="og:site_name" content="Salsa Rennes" />
+      
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={fullImageUrl} />
       <link rel="canonical" href={url} />
       {noIndex && <meta name="robots" content="noindex, nofollow" />}
     </Head>
