fb35d56c74647979625bc19d9deb8ff2df605bb4
Ericphoto35 <ericdu974@gmail.com>
2025-05-12 09:02:00 +0200

    Ajout de paramètres de version aux favicons pour forcer leur actualisation

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,14 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/images/logonoir.png" />
-        <link rel="apple-touch-icon" href="/images/logonoir.png" />
-        <link rel="shortcut icon" type="image/png" href="/images/logonoir.png" />
+        {/* Favicon standard dans la racine avec paramètre de version pour forcer l'actualisation */}
+        <link rel="icon" href="/favicon.ico?v=2" />
+        {/* Favicon PNG */}
+        <link rel="icon" type="image/png" href="/images/logonoir.png?v=2" />
+        {/* Pour les appareils Apple */}
+        <link rel="apple-touch-icon" href="/images/logonoir.png?v=2" />
+        {/* Pour la compatibilité avec les anciens navigateurs */}
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?v=2" />
       </Head>
       <body>
         <Main />
